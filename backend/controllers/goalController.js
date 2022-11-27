bbff3e23f69cb07c3c6a7dd1d4f572219487b471
Domains18 <gibsonsgibson88@gmail.com>
2022-11-27 23:45:33 +0300

    complete grud functionality

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -24,13 +24,28 @@ const setGoal = asyncHandler (async (req, res) =>{
 
 //update 
 const updateGoal = asyncHandler (async (req, res) => {
-    res.status(200).json({ message: `Update goals ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error('Not Found!')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+    res.status(200).json(updatedGoal);
 })
 
 
 // delete
 const deleteGoal = asyncHandler (async (req, res) =>{
-    res.status(200).json({message: `Delete goals ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal Not found')
+    }
+    await goal.remove();
+    res.status(200).json({id: req.params.id})
 })
 module.exports = {
     getGoals,
