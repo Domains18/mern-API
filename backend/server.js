c0a99fca09d4b97dfa5d100db14993f58ad7981b
Domains18 <gibsonsgibson@gmail.com>
2022-11-27 22:05:01 +0300

    connect database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 const express = require ('express');
-const dotenv = require ('dotenv').config();
+const path = require('path');
+const dotenv = require ('dotenv').config({ path: path.resolve(__dirname+'/.env')});
+const colors = require('colors');
 const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./Config/db')
 const port = process.env.PORT || 8000;
+
+
+//connect database
+connectDB()
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+console.log(process.env.DATABASE_URI)
 
 
 app.use('/api/goals', require('./routes/goalRoutes'))
