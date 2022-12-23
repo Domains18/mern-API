c091a56416c63788c3911431f21468a8b3923afb
Domains18 <gibsonsgibson88@gmail.com>
2022-12-23 17:37:08 +0300

    user user tsx auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,7 +35,8 @@ const registerUser = asyncHandler (async(req, res) =>{
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
         
     } else{
@@ -67,7 +68,16 @@ const loginUser = asyncHandler (async( req, res)=>{
 // acces public
 const aboutMe = asyncHandler (async (req, res)=>{
     res.json({message: 'about me'})
-})
+});
+
+
+//generate token 
+
+const generateToken = (id) =>{
+    return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
+        expiresIn: '30d',
+    })
+}
 module.exports = {
     registerUser,
     loginUser,
