87b03b8879f4744612f02a824b0045afb09008de
Domains18 <gibsonsgibson88@gmail.com>
2022-12-26 23:45:20 +0300

    register form

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -4,11 +4,15 @@ import { useState, useEffect } from 'react';
 
 
 
-
-
-
-
 function Register() {
+    const [ formData, setFormData ] = useState({
+        name: ' ',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const { name, email, password, password2} = formData
   return (
     <div>Register</div>
   )
