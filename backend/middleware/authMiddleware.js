33b262fea390dc12c0a86abd612f1a155fd7e929
Domains18 <gibsonsgibson88@gmail.com>
2022-12-26 09:41:17 +0300

    Authentication middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../Models/userModels');
+
+const protect = asyncHandler( async (req, res, next)=>{
+    let token
+    if ( req.headers.authorization & req.headers.authorization.startsWith('Bearer')){
+        try{
+            token = req.headers.authorization.split(' ')[1]
+            //token verification
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+            //get user from token
+            req.user = await User.findById(decoded.id).select('-password')
+            next()
+        }catch (error){
+            console.log(error);
+            res.status(401);
+            throw new Error('Unauthorized!')
+        }
+    };
+
+    if(!token){
+        res.status(401);
+        throw new Error(' No token found, Unauthorized Access')
+    }
+});
