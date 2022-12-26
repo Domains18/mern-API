043f79805dba52b597bbde5041075c8ded45c98c
Domains18 <gibsonsgibson88@gmail.com>
2022-12-26 11:57:57 +0300

    user route jwt protection

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -5,7 +5,7 @@ const Goal = require('../Models/goalsModel')
 //get
 const getGoals =  asyncHandler (async (req, res) => {
 
-    const goals = await Goal.find();
+    const goals = await Goal.find({ user: req.user.id});
     res.status(200).json(goals)
 
 })
@@ -17,7 +17,8 @@ const setGoal = asyncHandler (async (req, res) =>{
         throw new Error('Please, Add Text');
     }
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
     res.status(200).json(goal)
 })
