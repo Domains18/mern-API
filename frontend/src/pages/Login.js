bc4717c634148a43397561af64f95419d4480025
Domains18 <gibsonsgibson88@gmail.com>
2022-12-27 00:28:26 +0300

    login form validation frontend

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,64 @@
+import { useState, useEffect } from 'react';
+import { FaSignInAlt, FaUser } from 'react-icons/fa'
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
+
+
+
+function Register() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const {email, password, } = formData
+
+    const onChange = (e) => {
+        setFormData((prevState)=>({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+    const onSubmit = (e)=>{
+        e.preventDefault();
+    }
+    return (
+        <>
+            <section className="heading">
+                <h1>
+                    <FaSignInAlt /> Login
+                </h1>
+                <p>Login To Your Account</p>
+            </section>
+            <section className="form">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input type="text"
+                            className='form-control'
+                            id='email'
+                            name='email'
+                            value={email}
+                            onChange={onChange}
+                            placeholder="Enter Your Email"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className='form-control'
+                            id='password'
+                            name='password'
+                            value={password}
+                            onChange={onChange}
+                            placeholder="Enter Your Password"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className='btn btn-block'>Submit</button>
+                    </div>
+                </form>
+            </section>
+        </>
+    )
 }
 
-export default Login
+export default Register
