04e30a0bb4407b833640e6c95b422e79c75d5b10
Domains18 <gibsonsgibson88@gmail.com>
2022-12-28 23:25:30 +0300

    dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,21 +1,40 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm';
+import Spinner from '../components/Spinner'
+import { getGoals, reset } from '../features/goals/GoalSlice';
 
 
 
 function Dashboard() {
 
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  const { user } = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.auth);
+  const { goals , isLoading, isError, message } = useSelector((state)=> state.goals);
 
   useEffect(() =>{
+
+    if (isError){
+      console.log(message)
+    }
+
     if(!user){
       navigate('/login')
     }
-  }, [user, navigate])
+
+    dispatch(getGoals())
+
+    return () => {
+      dispatch(reset())
+    }
+  }, [user, navigate, isError,message, dispatch])
+
+  if (isLoading){
+    return <Spinner/>
+  }
 
 
   return (
