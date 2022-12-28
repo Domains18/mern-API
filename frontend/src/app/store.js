cf65fe5a36c8d91978a13b57aa0c7daefaf35c13
Domains18 <gibsonsgibson88@gmail.com>
2022-12-28 12:32:38 +0300

    user redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/Auth/authSlice';
+import goalReducer from '../features/goals/GoalSlice';
 
 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    goals: goalReducer,
   },
 });
