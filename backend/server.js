b03429fbbb5966860fbfb11030ab8a2e584bfd11
Domains18 <gibsonsgibson88@gmail.com>
2022-12-30 14:51:06 +0300

    deploy prep

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,17 @@ app.use(cors())
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
+//serve frontend
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build'))); //deployment
+
+    app.get('*', (req, res)=> res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res)=>{
+        res.send('Enviroment Not In production')
+    })
+}
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`server started on port ${port}`));
